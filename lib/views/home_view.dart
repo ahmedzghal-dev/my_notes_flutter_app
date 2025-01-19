03563b909ae59a5078a349a95483ad6e8da0a358
ahmedzghal-dev <ahmed.zghal1233@gmail.com>
2025-01-19 02:56:07 +0100

    my notes

---
+++ lib/views/home_view.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:my_notes/firebase_options.dart';
+import 'package:my_notes/views/login_view.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform,
+      ),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            // final user = FirebaseAuth.instance.currentUser;
+            // print(user);
+            // if (user?.emailVerified ?? false) {
+            //   return const Text('Done');
+            // } else {
+            //   return const VerifyEmailView();
+            // }
+            return LoginView();
+          default:
+            return const CircularProgressIndicator();
+        }
+      },
+    );
+  }
+}
